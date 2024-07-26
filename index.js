5848613b6782e4ee1afb6ee3e8d2fd458df1fbf6
unknown <chamberbankes@gmail.com>
2024-07-26 15:55:09 -0400

    add SVG class

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-const {Triangle, Circle, Square} = require('./lib/shapes');
-
+const { Triangle, Circle, Square } = require('./lib/shapes');
+const fs = require('fs');
 
 
 const questions = [
-
 {
 type: "input",
 name: "text",
 message: "Enter text for the SVG up to three characters",
 },
-
 {
 type: "input",
-name: "text-color",
+name: "textColor",
 message: "Enter a text color",
 },
-
 {
 type: "list",
 name: "shape",
 message: "Choose a shape for the SVG",
-Choices: ["Triangle","Circle", "Square"],
+choices: ["Triangle", "Circle", "Square"],
 },
-
 {
 type: "input",
-name: "shape-color",
+name: "shapeColor",
 message: "Enter a color for the shape"
 },
 ];
+
+class SVG{
+constructor(){
+this.svgshape = '';
+this.svgtext = '';
+        
+}
+render(){
+return  `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+${this.svgshape}
+${this.svgtext}</svg>`; 
+}
+
+setSvgText(text, color) {
+this.svgtext = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+}
+    
+setSvgShape(shape){
+this.svgshape = shape.render();
+}
+}
